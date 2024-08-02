d466c3749caeeb2b79fc9870923c127fa9224e32
Sami Cemek <ahmet.cemek24@ncf.edu>
2024-08-01 23:53:16 -0400

    Daily LeetCode Question Done

--- BinarySearch.py
+++ BinarySearch.py
@@ -21,3 +21,44 @@ class Solution:
         return -1       
         
 #___________________________________________________________________________
+
+# Date Log: 08/01/24
+# Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+# Difficulty: Medium
+# Qnumber = 153
+
+# Time Complexity: O(log n)
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        res = nums[0]
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            #if the given array is already sorted
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+            
+            m = (l + r) // 2
+            res = min(res, nums[m])
+            
+            if nums[m] >= nums[l]:
+                l = m + 1
+            else:
+                r = m - 1          
+        return res     
+
+#___________________________________________________________________________
+
+# Date Log: 08/01/24
+# Link: https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+# Difficulty: Medium
+# Qnumber = 33
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+
+#___________________________________________________________________________
