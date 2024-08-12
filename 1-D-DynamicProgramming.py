7f0a84243c9bade68ef1b7e34b65699d079a07af
Sami Cemek <71249060+ascemek@users.noreply.github.com>
2024-08-12 14:01:31 -0400

    Daily LeetCode Question Done

--- 1-D-DynamicProgramming.py
+++ 1-D-DynamicProgramming.py
@@ -7,15 +7,15 @@
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 3:
-            return n
-        n1, n2 = 2, 3
-
-        for i in range(4, n + 1):
-            temp = n1 + n2
-            n1 = n2
-            n2 = temp
-        return n2
+        
+        # DP - Bottom Up Solution. Time Complexity: O(N)
+        
+        oneStepPointer, twoStepPointer = 1, 1
+        for i in range(n - 1):
+            temp = oneStepPointer
+            oneStepPointer = oneStepPointer + twoStepPointer
+            twoStepPointer = temp
+        return oneStepPointer
     
 #___________________________________________________________________________
     
@@ -27,12 +27,70 @@ class Solution:
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        rob1, rob2 = 0, 0
-
+        # [rob1, rob2, n, n+1, ...]
+        
         for n in nums:
-            temp = max(n + rob1, rob2)
+            temp = max(rob1 + n, rob2)
             rob1 = rob2
             rob2 = temp
+            
         return rob2
+    
+#___________________________________________________________________________
+
+# Date Log: 08/12/24
+# Link: https://leetcode.com/problems/min-cost-climbing-stairs/description/
+# Difficulty: Easy
+# Qnumber = 746
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        # DP - Time Complexity: O(N), Space Complexity: O(1)
+        
+        cost.append(0)
+        
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+            
+        return min(cost[0], cost[1])
+        
+#___________________________________________________________________________
+
+# Date Log: 08/12/24
+# Link: https://leetcode.com/problems/house-robber-ii/description/
+# Difficulty: Medium
+# Qnumber = 213
+
+# Time Complexity: O(N), Space Complexity: O(1)
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
+        
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+        
+        for n in nums:
+            newRob = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = newRob
+            
+        return rob2
+#___________________________________________________________________________
+
+# Date Log: 08/12/24
+# Link: https://leetcode.com/problems/house-robber-iii/description/
+# Difficulty: Medium
+# Qnumber = 337
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
 
 #___________________________________________________________________________
