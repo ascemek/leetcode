9ad06fe465e7cae104eb480f9ed5f37679da2c18
Sami Cemek <71249060+ascemek@users.noreply.github.com>
2024-08-14 14:08:18 -0400

    Daily LeetCode Question Done

--- BinarySearch.py
+++ BinarySearch.py
@@ -101,4 +101,69 @@ class TimeMap:
         
 #___________________________________________________________________________
 
+# Date Log: 08/14/24
+# Link: https://leetcode.com/problems/koko-eating-bananas/description/
+# Difficulty: Medium
+# Qnumber = 875
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        l, r = math.ceil(sum(piles) / h), max(piles)
+        res = r
+        
+        while l <= r:
+            
+            k = (l + r) // 2
+            hours = 0
+            
+            for p in piles:
+                hours += math.ceil(p / k)
+                
+            if hours <= h:
+                res = min(res, k)
+                r = k - 1
+            else:
+                l = k + 1
+        return res
+
+#___________________________________________________________________________
+
+# Date Log: 08/14/24
+# Link: https://leetcode.com/problems/search-a-2d-matrix/description/
+# Difficulty: Medium
+# Qnumber = 74
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        # Time Complexity: log(n) + log(m)
+        
+        ROWS, COLS = len(matrix), len(matrix[0])
+        top, bot = 0, ROWS - 1
+        
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        
+        if not (top <= bot):
+            return False
+        
+        l, r = 0, COLS - 1
+        
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+        return False
+        
 #___________________________________________________________________________
--- Stack.py
+++ Stack.py
@@ -127,4 +127,20 @@ class Solution:
 
 #___________________________________________________________________________
         
+# Date Log: 08/14/24
+# Link: https://leetcode.com/problems/car-fleet/description/
+# Difficulty: Medium
+# Qnumber = 853
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         
+        pair = [[p, s] for p, s in zip(position, speed)]
+        stack = []
+        
+        for p, s in sorted(stack)[::-1]:
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-2] >= stack[-1]:
+                stack.pop()
+        return len(stack)
+#___________________________________________________________________________
