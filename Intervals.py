45adaf4fed021561192d32e58272182b8d1b414e
Sami Cemek <71249060+ascemek@users.noreply.github.com>
2024-09-05 23:36:34 -0400

    Daily LeetCode Question Done.

--- Intervals.py
+++ Intervals.py
@@ -26,3 +26,72 @@ class Solution:
         return res
 
 #___________________________________________________________________________
+
+# Date Log: 09/05/24
+# Link: https://leetcode.com/problems/merge-intervals/description/
+# Difficulty: Medium
+# Qnumber = 56
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        # Time Complexity: O(nlogn)
+        
+        intervals.sort()
+        res = [intervals[0]]
+        
+        for start, end in intervals[1:]:
+            lastEnd = res[-1][1]
+            
+            if start <= lastEnd:
+                res[-1][1] = max(lastEnd, end)
+            else:
+                res.append([start, end])
+                
+        return res
+
+#___________________________________________________________________________
+
+# Date Log: 09/05/24
+# Link: https://leetcode.com/problems/non-overlapping-intervals/description/
+# Difficulty: Medium
+# Qnumber = 435
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        
+        # Time Complexity: O(nlogn)
+        
+        intervals.sort()
+        res = 0
+        
+        prevEnd = intervals[0][1]
+        for start, end in intervals[1:]:
+            if start >= prevEnd:
+                prevEnd = end
+            else:
+                res += 1
+                prevEnd = min(prevEnd, end)
+                
+        return res
+            
+#___________________________________________________________________________
+
+# Date Log: 09/05/24
+# Link: https://neetcode.io/problems/meeting-schedule
+# Difficulty: Easy
+# Qnumber = 920
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        
+
+#___________________________________________________________________________
