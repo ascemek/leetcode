09349567705f5641c2248888100eb05715dc93a2
Sami Cemek <71249060+ascemek@users.noreply.github.com>
2024-09-10 23:55:42 -0400

    Daily LeetCode Question Done

--- Greedy.py
+++ Greedy.py
@@ -131,5 +131,28 @@ class Solution:
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
+        
+        lastIndex = {} # char -> lastIndex in s
+        
+        for i, c in enumerate(s):
+            lastIndex[c] = i
+        
+        size, end = 0
+        res = []
+        for i, c in enumerate(s):
+            size += 1
+            end = max(end, lastIndex[c])
+            
+            if i == end:
+                res.append(size)
+                size = 0
+        return res
+    
+#___________________________________________________________________________
+
+# Date Log: 09/10/24
+# Link: 
+# Difficulty: Medium
+# Qnumber = 
 
 #___________________________________________________________________________
